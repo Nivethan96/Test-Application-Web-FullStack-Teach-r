5ba0a53ee874140657714755c785f037163fc8f3
Nivethan96 <78319817+Nivethan96@users.noreply.github.com>
2021-02-21 19:09:02 +0100

    Test 2 : Show data in web page (failed)

--- frontend-react-native/components/Data.js
+++ frontend-react-native/components/Data.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, View, Text, FlatList, TouchableHighlight } from 'react-native';
-import Axios from 'axios';
+import { StyleSheet, View, Text, FlatList, TouchableHighlight, ListItem } from 'react-native';
 import axios from 'axios';
 
 export default function DataContainer() {
-    let [user, getUser] = React.useState('');
+    let [user, setUser] = React.useState([]);
     const url = 'http://172.20.10.6:8000/';
 
     useEffect(() => {
@@ -14,18 +13,29 @@ export default function DataContainer() {
     const getData = () => {
         axios.get(`${url}show-users`)
             .then((res) => {
-                console.warn(res)
-                // const allUsers = res.data.prenom;
-                // getUser(allUsers);
+                console.warn(res.data)
+                setUser(res.data.prenom)
             })
             .catch((err) => {
                 console.log(err)
             })
+
+    }
+
+    //if (!user) { return null; }
+
+    const renderUser = ({ item }) => {
+        return (
+            <ListItem>
+                <Text>{item.prenom}</Text>
+            </ListItem>
+        )
     }
     return (
         <View>
-            <Text> mock data</Text>
+            <FlatList data={user}
+                renderItem={renderUser}
+            />
         </View>
-    )
-
+    );
 }
